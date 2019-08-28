508dfe6b038cb5afca6ad8303532be972630c7ea
Mathias Reck <mathias.reck@gmail.com>
2019-08-29 00:10:51 +0200

    basic setup

--- link.sh
+++
@@ -1,3 +0,0 @@
-ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sfv ~/dotfiles/.vimrc ~/.vimrc
-
---
+++ setup.sh
@@ -0,0 +1,3 @@
+ln -sfv $HOME/configs/.tmux.conf $HOME/tmux.conf
+ln -sfv $HOME/configs/.vimrc $HOME/vimrc
+
