5c8e8eefaca71d6fd5160eb363516a1f490e3275
Mathias Reck <mathias.reck@gmail.com>
2020-03-29 22:59:14 +0200

    remove dots from directories

--- setup.sh
+++ setup.sh
@@ -8,19 +8,19 @@ DOTFILES=$HOME/dotfiles
 # vim
 mkdir -p $HOME/.vim > /dev/null
 mkdir -p $HOME/.cache/vim/undo > /dev/null
-ln -sfv $DOTFILES/.vim/vimrc $HOME/.vim/vimrc
-ln -sfv $DOTFILES/.vim/autoload $HOME/.vim/
+ln -sfv $DOTFILES/vim/vimrc $HOME/.vim/vimrc
+ln -sfv $DOTFILES/vim/autoload $HOME/.vim/
 
 ln -sfv $DOTFILES/.tmux.conf $HOME/.tmux.conf
 
 # alacritty
 mkdir -p $HOME/.config/alacritty
-ln -sfv $DOTFILES/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+ln -sfv $DOTFILES/config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
 ln -sfv $DOTFILES/.zshrc $HOME/.zshrc
 ln -sfv $DOTFILES/.zprofile $HOME/.zprofile
-ln -sfv $DOTFILES/.config/i3 $HOME/.config
-ln -sfv $DOTFILES/.config/fish $HOME/.config
+ln -sfv $DOTFILES/config/i3 $HOME/.config
+ln -sfv $DOTFILES/config/fish $HOME/.config
 ln -sfv $DOTFILES/.bashrc $HOME/.bashrc
 ln -sfv $DOTFILES/emacs/init.el $HOME/.emacs.d/init.el
 ln -sfv $DOTFILES/emacs/init-packages.el $HOME/.emacs.d/init-packages.el
