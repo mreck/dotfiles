d2eca4936b1c215de201c34bf27b5a0055179d24
Mathias Reck <mathias.reck@gmail.com>
2020-04-18 12:07:19 +0200

    try move custom keybindings for emacs

--- emacs/init.el
+++ emacs/init.el
@@ -7,14 +7,40 @@
 (line-number-mode 1)
 (column-number-mode 1)
 
+;; fix UI
 (blink-cursor-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
 
+;; custom keys
+(global-set-key (kbd "C-x")       'kill-region)            ; cut
+(global-set-key (kbd "C-c")       'kill-ring-save)         ; copy
+(global-set-key (kbd "C-v")       'yank)                   ; paste
+(global-set-key (kbd "C-z")       'undo)                   ; undo
+(global-set-key (kbd "C-y")       'redo)                   ; rendo
+
+(global-set-key (kbd "C-f")       'forward-search)         ; search forward
+(global-set-key (kbd "<home>")    'move-beginning-of-line) ; goto: beginning of line
+(global-set-key (kbd "<end>")     'move-end-of-line)       ; goto: end of line
+
+(global-set-key (kbd "C-o")       'find-file)              ; open file
+(global-set-key (kbd "C-s")       'save-buffer)            ; save current buffer
+(global-set-key (kbd "C-S-s")     'save-some-buffer)       ; save all open buffers
+(global-set-key (kbd "C-w")       'kill-buffer)            ; close current buffers
+(global-set-key (kbd "C-r")       'recentf-open-files)     ; recent files
+
+(global-set-key (kbd "<mouse-2>") 'clipboard-yank)         ; paste from clipboard
+
+
 ;; prevent Emacs from losing undo information
 (setq undo-limit 2000000)
 (setq undo-strong-limit 4000000)
+
+;; recent files
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
 
 ;; turn on highlight matching brackets when cursor is on one
 (show-paren-mode t)
